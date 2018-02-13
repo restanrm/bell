530e22649bf4a99aada8b46255cbbf11c9051e1b
Adrien RAFFIN <adrien.raffin@amossys.fr>
2018-02-13 23:07:04 +0100

    Add player of sounds

--- front/src/components/List.vue
+++ front/src/components/List.vue
@@ -1,39 +1,59 @@
 <template>
   <div class="list">
-    <h1>{{ msg }}</h1>
+    <div class="jumbotron">
+      <div class="container">
+        <div class="row">
+          <div class="col" v-for="sound in sounds"><button class="btn btn-primary" v-on:click="play(sound.name)">{{sound.name}}</button></div>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-export default {
-  name: 'list',
-  data () {
-    this.$http.get('/api/v1/sounds').then(response => {
+  export default {
+    name: 'list',
+    data () {
       return {
-        msg: response.body;
+        sounds: [],
+        playURL: '/api/v1/play/',
+        soundsURL: '/api/v1/sounds'
       }
-    })
+    },
+    methods: {
+      play: function (sound) {
+        this.$http.get(this.playURL + sound)
+      },
+      updateSounds: function () {
+        this.$http.get(this.soundsURL).then(response => {
+          this.sounds = response.data
+        })
+      }
+    },
+    created: function () {
+      this.updateSounds()
+    }
   }
-}
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style>
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #35495E;
-}
+  h1,
+  h2 {
+    font-weight: normal;
+  }
+  
+  ul {
+    list-style-type: none;
+    padding: 0;
+  }
+  
+  li {
+    display: inline-block;
+    margin: 0 10px;
+  }
+  
+  a {
+    color: #35495E;
+  }
 </style>
