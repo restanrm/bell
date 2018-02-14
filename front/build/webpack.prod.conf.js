95bdc417cba484f557adadb86b762c0352f3bd43
Adrien RAFFIN <adrien.raffin@sekoia.fr>
2018-02-14 16:07:29 +0100

    Rename from front to Bell

--- front/build/webpack.prod.conf.js
+++ front/build/webpack.prod.conf.js
@@ -104,7 +104,7 @@ const webpackConfig = merge(baseWebpackConfig, {
     ]),
     // service worker caching
     new SWPrecacheWebpackPlugin({
-      cacheId: 'front',
+      cacheId: 'bell',
       filename: 'service-worker.js',
       staticFileGlobs: ['dist/**/*.{js,html,css}'],
       minify: true,
