b6b2ba7f12bf564954af8882337a1de66824ac20
Adrien RAFFIN <adrien.raffin@amossys.fr>
2018-02-26 23:27:10 +0100

    Activate router and add navbar

--- front/src/App.vue
+++ front/src/App.vue
@@ -1,7 +1,19 @@
 <template lang="jade">
   .app
-    header
-      span Bell
+    nav.navbar.navbar-toggleable-md
+      button.navbar-toggler.custom-toggler(type="button",
+        data-toggle="collapse",
+        data-target="#navbarSupportedContent",
+        aria-controls="navbarSupportedContent",
+        aria-expanded="false",
+        aria-label="Toggle navigation",
+        )
+        span.custom-toggler
+          i.fa.fa-bars.white
+      a.navbar-brand(href="#") Bell
+      .collapse.navbar-collapse#navbarSupportedContent
+        router-link.nav-item.nav-link.active(to="/") List
+        router-link.nav-item.nav-link(to="/upload") Upload
     router-view
 </template>
 
@@ -25,7 +37,21 @@
     -moz-osx-font-smoothing: grayscale;
     color: #2c3e50;
   }
-  
+
+  .navbar, .navbar-brand {
+    background: $secondary-light !important;
+    color: white;
+    a, .navbar-toggler-icon {
+      color: white;
+    }
+  }
+
+  .custom-toggler{
+    border-color: white;
+    // background: white;
+    color: white;
+  }
+
   main {
     text-align: center;
     margin-top: 40px;
