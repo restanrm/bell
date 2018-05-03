e57fec90bba7ac76fe0af7068ebd01aa20c1e630
Adrien RAFFIN <adrien.raffin@sekoia.fr>
2018-05-03 09:20:44 +0200

    refact(bellctl) update the add with a function

--- cmd/bellctl/cmd/add.go
+++ cmd/bellctl/cmd/add.go
@@ -27,67 +27,8 @@ var addCmd = &cobra.Command{
 		soundName := cmd.Flag("name").Value.String()
 		soundFile := cmd.Flag("file").Value.String()
 
-		address, err := url.Parse(viper.GetString("bell.address") + AddSoundPath)
+		err := add(soundName, soundFile, tagsList...)
 		if err != nil {
-			logrus.WithFields(logrus.Fields{
-				"error":          err,
-				"server address": viper.GetString("bell.address"),
-				"method":         "addCmd.Run",
-			}).Error("Failed to build url")
-			return
-		}
-
-		bodyBuf := &bytes.Buffer{}
-		bodyWriter := multipart.NewWriter(bodyBuf)
-
-		fileWriter, err := bodyWriter.CreateFormFile("uploadFile", soundFile) // this name shoud be seen later
-
-		fh, err := os.Open(soundFile)
-		if err != nil {
-			logrus.WithFields(logrus.Fields{
-				"error":    err,
-				"filename": soundFile,
-			}).Error("Failed to read the given file")
-			return
-		}
-		defer fh.Close()
-
-		_, err = io.Copy(fileWriter, fh)
-		if err != nil {
-			logrus.WithFields(logrus.Fields{
-				"error": err,
-			}).Error("Failed to copy content of file to request writer")
-			return
-		}
-
-		bodyWriter.WriteField("name", soundName)
-		contentType := bodyWriter.FormDataContentType()
-		bodyWriter.Close()
-
-		// add tags to the query if tag list is more than nothing
-		if len(tagsList) > 0 {
-			values := address.Query()
-			for _, tag := range tagsList {
-				values.Add("tag", tag)
-			}
-			address.RawQuery = values.Encode()
-		}
-
-		resp, err := http.Post(address.String(), contentType, bodyBuf)
-		if err != nil {
-			logrus.WithFields(logrus.Fields{
-				"error": err,
-			}).Error("Failed to send data to bell server")
-			return
-		}
-		defer resp.Body.Close()
-
-		if resp.StatusCode > 299 {
-			content, _ := ioutil.ReadAll(resp.Body)
-			logrus.WithFields(logrus.Fields{
-				"status_code": resp.StatusCode,
-				"body":        string(content),
-			}).Info("Failed to add new sound to bell server")
 			return
 		}
 		logrus.Info("Sound successfully uploaded")
@@ -106,4 +47,71 @@ func init() {
 	addCmd.MarkFlagRequired("file")
 
 	addCmd.Flags().StringSliceVarP(&tagsList, "tags", "t", []string{}, "List of tags to add to the sound")
+}
+
+func add(file, name string, tags ...string) (err error) {
+	address, err := url.Parse(viper.GetString("bell.address") + AddSoundPath)
+	if err != nil {
+		logrus.WithFields(logrus.Fields{
+			"error":          err,
+			"server address": viper.GetString("bell.address"),
+			"method":         "addCmd.Run",
+		}).Error("Failed to build url")
+		return
+	}
+
+	bodyBuf := &bytes.Buffer{}
+	bodyWriter := multipart.NewWriter(bodyBuf)
+
+	fileWriter, err := bodyWriter.CreateFormFile("uploadFile", file) // this name shoud be seen later
+
+	fh, err := os.Open(file)
+	if err != nil {
+		logrus.WithFields(logrus.Fields{
+			"error":    err,
+			"filename": file,
+		}).Error("Failed to read the given file")
+		return
+	}
+	defer fh.Close()
+
+	_, err = io.Copy(fileWriter, fh)
+	if err != nil {
+		logrus.WithFields(logrus.Fields{
+			"error": err,
+		}).Error("Failed to copy content of file to request writer")
+		return
+	}
+
+	bodyWriter.WriteField("name", name)
+	contentType := bodyWriter.FormDataContentType()
+	bodyWriter.Close()
+
+	// add tags to the query if tag list is more than nothing
+	if len(tags) > 0 {
+		values := address.Query()
+		for _, tag := range tags {
+			values.Add("tag", tag)
+		}
+		address.RawQuery = values.Encode()
+	}
+
+	resp, err := http.Post(address.String(), contentType, bodyBuf)
+	if err != nil {
+		logrus.WithFields(logrus.Fields{
+			"error": err,
+		}).Error("Failed to send data to bell server")
+		return
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode > 299 {
+		content, _ := ioutil.ReadAll(resp.Body)
+		logrus.WithFields(logrus.Fields{
+			"status_code": resp.StatusCode,
+			"body":        string(content),
+		}).Info("Failed to add new sound to bell server")
+		return
+	}
+	return
 }
